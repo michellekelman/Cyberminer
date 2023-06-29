a0bea1f407913ab6c74c5b030c5bdec34e9c9fa4
Michelle Kelman <78247410+michellekelman@users.noreply.github.com>
2023-06-28 23:49:54 -0500

    add crawler docs

--- static/pagination.js
+++ static/pagination.js
@@ -11,21 +11,27 @@ function enableButton(button) {
 }
 
 function edgeButtonControl() {
+    const firstButton = document.getElementById("first-button");
+    const lastButton = document.getElementById("last-button");
     const prevButton = document.getElementById("prev-button");
     const nextButton = document.getElementById("next-button");
     if (currentPage == 1) {
+        disableButton(firstButton);
         disableButton(prevButton);
     }
     else {
+        enableButton(firstButton);
         enableButton(prevButton);
     }
     const oneDoc = document.querySelectorAll(".one-doc");
     const resultsNum = document.getElementById("results-num").value;
     const pageCount = Math.ceil(oneDoc.length / resultsNum);
     if (currentPage == pageCount) {
+        disableButton(lastButton);
         disableButton(nextButton);
     }
     else {
+        enableButton(lastButton);
         enableButton(nextButton);
     }
 }
@@ -41,10 +47,52 @@ function setActivePageButton() {
     }
 }
 
+function displayButtonRange() {
+    const pageNumbers = document.querySelectorAll(".page-number");
+    const oneDoc = document.querySelectorAll(".one-doc");
+    const resultsNum = document.getElementById("results-num").value;
+    const pageCount = Math.ceil(oneDoc.length / resultsNum);
+    const dotButton1 = document.getElementById("dot-button-1");
+    const dotButton2 = document.getElementById("dot-button-2");
+    for (let i = 0; i < pageNumbers.length; i++) {
+        pageNumbers[i].classList.remove("d-none");
+    }
+    dotButton1.classList.add("d-none");
+    dotButton2.classList.add("d-none");
+    if (pageCount > 10) {
+        if (currentPage < 5) {
+            for (let i = 0; i < pageNumbers.length; i++) {
+                if (i > 4) {
+                    pageNumbers[i].classList.add("d-none");
+                }
+            }
+            dotButton2.classList.remove("d-none");
+        }
+        else if (currentPage > pageCount-4) {
+            for (let i = 0; i < pageNumbers.length; i++) {
+                if (i < pageCount-5) {
+                    pageNumbers[i].classList.add("d-none");
+                }
+            }
+            dotButton1.classList.remove("d-none");
+        }
+        else {
+            for (let i = 0; i < pageNumbers.length; i++) {
+                if (i < currentPage-3 || i > currentPage+1) {
+                    pageNumbers[i].classList.add("d-none");
+                }
+            }
+            dotButton1.classList.remove("d-none");
+            dotButton2.classList.remove("d-none");
+        }
+    }
+}
+
 function appendPageButton(index) {
     const pageNumbers = document.getElementById("page-numbers");
     const pageNumberLi = document.createElement("li");
     pageNumberLi.className = "page-item";
+    pageNumberLi.classList.add("page-number");
     pageNumberLi.value = index;
     const pageNumberA = document.createElement("a");
     pageNumberA.className = "page-link";
@@ -54,8 +102,17 @@ function appendPageButton(index) {
 }
 
 function appendPageButtons() {
-    // prev button
+    // first button
     const pageNumbers = document.getElementById("page-numbers");
+    const pageNumberLiFirst = document.createElement("li");
+    pageNumberLiFirst.className = "page-item";
+    pageNumberLiFirst.id = "first-button";
+    const pageNumberAFirst = document.createElement("a");
+    pageNumberAFirst.className = "page-link";
+    pageNumberAFirst.innerHTML = "&lt&lt";
+    pageNumberLiFirst.appendChild(pageNumberAFirst);
+    pageNumbers.appendChild(pageNumberLiFirst);
+    // prev button
     const pageNumberLiPrev = document.createElement("li");
     pageNumberLiPrev.className = "page-item";
     pageNumberLiPrev.id = "prev-button";
@@ -64,6 +121,16 @@ function appendPageButtons() {
     pageNumberAPrev.innerHTML = "&lt";
     pageNumberLiPrev.appendChild(pageNumberAPrev);
     pageNumbers.appendChild(pageNumberLiPrev);
+    // ... button 1
+    const pageNumberLiDot1 = document.createElement("li");
+    pageNumberLiDot1.className = "page-item";
+    pageNumberLiDot1.id = "dot-button-1";
+    const pageNumberADot1 = document.createElement("a");
+    pageNumberADot1.className = "page-link";
+    pageNumberADot1.innerHTML = "...";
+    pageNumberLiDot1.appendChild(pageNumberADot1);
+    pageNumbers.appendChild(pageNumberLiDot1);
+    disableButton(pageNumberLiDot1);
     // middle buttons
     const oneDoc = document.querySelectorAll(".one-doc");
     const resultsNum = document.getElementById("results-num").value;
@@ -71,6 +138,16 @@ function appendPageButtons() {
     for (let i = 1; i <= pageCount; i++) {
         appendPageButton(i);
     }
+    // ... button 2
+    const pageNumberLiDot2 = document.createElement("li");
+    pageNumberLiDot2.className = "page-item";
+    pageNumberLiDot2.id = "dot-button-2";
+    const pageNumberADot2 = document.createElement("a");
+    pageNumberADot2.className = "page-link";
+    pageNumberADot2.innerHTML = "...";
+    pageNumberLiDot2.appendChild(pageNumberADot2);
+    pageNumbers.appendChild(pageNumberLiDot2);
+    disableButton(pageNumberLiDot2);
     // next button
     const pageNumberLiNext = document.createElement("li");
     pageNumberLiNext.className = "page-item";
@@ -80,6 +157,15 @@ function appendPageButtons() {
     pageNumberANext.innerHTML = "&gt";
     pageNumberLiNext.appendChild(pageNumberANext);
     pageNumbers.appendChild(pageNumberLiNext);
+    // last button
+    const pageNumberLiLast = document.createElement("li");
+    pageNumberLiLast.className = "page-item";
+    pageNumberLiLast.id = "last-button";
+    const pageNumberALast = document.createElement("a");
+    pageNumberALast.className = "page-link";
+    pageNumberALast.innerHTML = "&gt&gt";
+    pageNumberLiLast.appendChild(pageNumberALast);
+    pageNumbers.appendChild(pageNumberLiLast);
 }
 
 function resetPageButtons() {
@@ -93,6 +179,7 @@ function displayCurrentPageResults(pageNum) {
     currentPage = pageNum;
     edgeButtonControl();
     setActivePageButton();
+    displayButtonRange();
     const resultsNum = document.getElementById("results-num").value;
     const prevRange = (pageNum - 1) * resultsNum;
     const currRange = pageNum * resultsNum;
@@ -109,6 +196,17 @@ function loadPagination() {
     resetPageButtons();
     appendPageButtons();
     displayCurrentPageResults(1);
+    const firstButton = document.getElementById("first-button");
+    const lastButton = document.getElementById("last-button");
+    const oneDoc = document.querySelectorAll(".one-doc");
+    const resultsNum = document.getElementById("results-num");
+    const pageCount = Math.ceil(oneDoc.length / resultsNum.value);
+    firstButton.addEventListener("click", () => {
+        displayCurrentPageResults(1);
+    });
+    lastButton.addEventListener("click", () => {
+        displayCurrentPageResults(pageCount);
+    });
     const prevButton = document.getElementById("prev-button");
     const nextButton = document.getElementById("next-button");
     prevButton.addEventListener("click", () => {
@@ -126,7 +224,6 @@ function loadPagination() {
             });
         };
     };
-    const resultsNum = document.getElementById("results-num");
     resultsNum.addEventListener("change", () => {
         loadPagination();
     });
