c6b418c6746f977c4192dbb39bd5aa41a075131e
Michelle Kelman <78247410+michellekelman@users.noreply.github.com>
2023-07-06 17:03:29 -0500

    todo items

--- controllers/controller.js
+++ controllers/controller.js
@@ -17,14 +17,19 @@ exports.searchDocs = async (req, res) => {
         queryStrings = queryString.split(" ");
     }
 
-    // console.log(queryString, select, sort);
+    // DEBUG - console.log(queryString, select, sort);
 
     // TODO - query based on queryStrings - find()
     // TODO - filter with and, or, not search - find()
     // TODO - sort with alph, freq, len - sort()
-    // TODO - implement frequency tracker
-
     const cursor = client.client.db("Cluster0").collection("documents").find().sort();
+
     const data = await cursor.toArray();
     res.render("./index", {"docs": data, "formContents": [queryString, select, sort]});
+}
+
+function clicked() {
+    // TODO - implement frequency tracker
+    // implement function here - findOne() with url as id and update() in MongoDb
+    // connect to onClick() in html
 }
